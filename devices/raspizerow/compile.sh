b68103a32ed99debec3d93216d8f87ab327bd1bc
Michael Russell <roocell@gmail.com>
2024-02-17 15:23:17 +0000

    raspizerow main to accept args to save audio to local file and apply gain. required introducing CMake

--- devices/raspizerow/compile.sh
+++ devices/raspizerow/compile.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-sudo g++ -std=c++17 main.cpp -g -o main -lasound -lcurl -lpthread
+
+# Create a build directory
+mkdir -p build
+cd build
+
+# Generate build files using CMake
+cmake ..
+
+# Build the project using make
+make
+
+cp -f main ..
